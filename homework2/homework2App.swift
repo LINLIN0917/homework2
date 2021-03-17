3beb90190e4554daed225f79065e642fbdba2795
Linlin <chenglin353163@gmail.com>
2021-03-18 00:28:45 +0800

    Initial Commit

---
+++ homework2/homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  homework2App.swift
+//  homework2
+//
+//  Created by Linlin on 2021/3/18.
+//
+
+import SwiftUI
+
+@main
+struct homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
